3dc7ac4bdf140e5dc627dc171497f4ab999c9bc6
CliffCrerar <cliff.crerar@gmail.com>
2023-06-15 17:18:45 +0200

    3 files modified, 1 added

---
+++ src/app/ng-logo.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'int-logo',
+  template: `
+  <div>
+    <a
+      href="https://www.behance.net/gallery/105201671/Interpreted-Dev-Hero/modules/604051337"
+      target="_blank"
+    >
+      <img
+        class="img-fluid w-25 m-auto d-block"
+        src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/679761105201671.5f73c82211ed0.jpg"
+      />
+    </a>
+  </div>
+  `,
+})
+export class NgInterpretedLogo {}
